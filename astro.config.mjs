d1c72d968d948d58fe2b4545987160482c01c68c
Alan Vidal <alanvidal.c@gmail.com>
2024-06-04 11:27:25 -0400

    fix: change to a hybrid output SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://alan-vidal.github.io",
   base: "APP-Public",
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
